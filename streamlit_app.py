7485d9ba772f8bca5c187cc86520b35fadc2aa7f
stefangithub23 <148984635+stefangithub23@users.noreply.github.com>
2023-10-29 18:14:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega')
+streamlit.text ('🥣 🥗 🐔 🥑🍞Omega')
 streamlit.text ('Kale')
 streamlit.text ('Egg')
