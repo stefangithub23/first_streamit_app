8157dc1360978a3d77ed87fbf77d375d95d45fdf
stefangithub23 <148984635+stefangithub23@users.noreply.github.com>
2023-11-01 22:11:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+import requests
+
+fruityvice_response = requests.get"https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+
+
+
+
 
 #streamlit.text(fruityvice_response.json())
 
